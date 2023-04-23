c5855e61d72ad634702aa58e9591244c7bf9d7ad
Mithlesh <mithleshgupta241@gmail.com>
2023-04-23 14:48:45 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 100);
+});
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    const icon = faq.querySelector(".faq-icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "block";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+const closNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closNav);
